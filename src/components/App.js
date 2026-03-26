546a108e10d934538ad56ce8c59168a1e9bb8699
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2026-03-26 12:18:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,39 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
+function LoginForm({ isLoggedIn, onLogin }){
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    onLogin();
+  };
+  return(
     <div>
+  {isLoggedIn ? (
+    <form onSubmit={handleSubmit}>
+     <h3>Parent Component</h3>
+     <label>Username</label>
+     <input type="text" placeholder="Username" required />
+
+     <label>Password</label>
+     <input type="password" placeholder="Password" required />
+
+     <button type="submit">Login</button>
+    </form>
+  ) : (
+    <h2>You are Logged in!</h2>
+  )}
+</div>
+);
+}
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useStatae(false);
+
+  const handleLogin=()=>{
+    setIsLoggedIn(true);
+  }
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <LoginForm isLoggedIn={isLoggedIn} onLogin={handleLogin} />
         {/* Do not remove the main div */}
     </div>
   )
